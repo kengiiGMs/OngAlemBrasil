22fba6de02dbed477081bcaba8d46568294060f7
KengiiGMs <kaikiaoto22@gmail.com>
2022-05-30 14:09:48 -0300

    MenuBottom retirado

--- ong_alem_brasil/lib/homePage.dart
+++ ong_alem_brasil/lib/homePage.dart
@@ -1,4 +1,3 @@
-import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
@@ -22,7 +21,6 @@ class _HomePageState extends State<HomePage> {
         child: AppBar(
            shape: RoundedRectangleBorder(borderRadius: BorderRadius.only( bottomRight: Radius.circular(30))),
           centerTitle: true,
-          leading: MenuDrop(), 
           flexibleSpace: 
             Container(
               decoration: BoxDecoration(
@@ -69,122 +67,5 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
         );
-  }
-}
-
-
-class MenuDrop extends StatefulWidget {
-  const MenuDrop({ Key? key }) : super(key: key);
-
-  @override
-  State<MenuDrop> createState() => _MenuDropState();
-}
-
-class _MenuDropState extends State<MenuDrop> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-     child: DropdownButtonHideUnderline(
-          child: DropdownButton2(
-            customButton: const Icon(
-              Icons.list,
-              size: 46,
-              color: Colors.red,
-            ),
-            customItemsIndexes: const [3],
-            customItemsHeight: 8,
-            items: [
-              ...MenuItems.firstItems.map(
-                        (item) =>
-                        DropdownMenuItem<MenuItem>(
-                          value: item,
-                          child: MenuItems.buildItem(item),
-                        ),
-              ),
-              const DropdownMenuItem<Divider>(enabled: false, child: Divider()),
-              ...MenuItems.secondItems.map(
-                        (item) =>
-                        DropdownMenuItem<MenuItem>(
-                          value: item,
-                          child: MenuItems.buildItem(item),
-                        ),
-              ),
-            ],
-            onChanged: (value) {
-              MenuItems.onChanged(context, value as MenuItem);
-            },
-            itemHeight: 48,
-            itemPadding: const EdgeInsets.only(left: 16, right: 16),
-            dropdownWidth: 160,
-            dropdownPadding: const EdgeInsets.symmetric(vertical: 6),
-            dropdownDecoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(4),
-              color: Colors.redAccent,
-            ),
-            dropdownElevation: 8,
-            offset: const Offset(0, 8),
-          ),
-        ),
-      );
-  }
-}
-
-
-
- class MenuItem {
-  final String text;
-  final IconData icon;
-
-  const MenuItem({
-    required this.text,
-    required this.icon,
-  });
-}
-
-class MenuItems {
-  static const List<MenuItem> firstItems = [home, share, settings];
-  static const List<MenuItem> secondItems = [logout];
-
-  static const home = MenuItem(text: 'Home', icon: Icons.home);
-  static const share = MenuItem(text: 'Share', icon: Icons.share);
-  static const settings = MenuItem(text: 'Settings', icon: Icons.settings);
-  static const logout = MenuItem(text: 'Log Out', icon: Icons.logout);
-
-  static Widget buildItem(MenuItem item) {
-    return Row(
-      children: [
-        Icon(
-                item.icon,
-                color: Colors.white,
-                size: 22
-        ),
-        const SizedBox(
-          width: 10,
-        ),
-        Text(
-          item.text,
-          style: const TextStyle(
-            color: Colors.white,
-          ),
-        ),
-      ],
-    );
-  }
-
-  static onChanged(BuildContext context, MenuItem item) {
-    switch (item) {
-      case MenuItems.home:
-      //Do something
-        break;
-      case MenuItems.settings:
-      //Do something
-        break;
-      case MenuItems.share:
-      //Do something
-        break;
-      case MenuItems.logout:
-      //Do something
-        break;
-    }
   }
 }
