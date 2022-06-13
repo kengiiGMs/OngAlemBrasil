96b51bd39e662236dd66551b4be9ba8c2446ed5e
KengiiGMs <kaikiaoto22@gmail.com>
2022-06-13 11:58:48 -0300

    Correção de Largura da pagina Perfik

--- ong_alem_brasil/lib/Profile/ProfilePage.dart
+++ ong_alem_brasil/lib/Profile/ProfilePage.dart
@@ -32,99 +32,105 @@ class _ProfilePageState extends State<ProfilePage> {
                       bottomLeft: Radius.circular(20),
                     ),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(25.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 5),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Column(
+                  child: Container(
+                    width: 800,
+                    child: Padding(
+                      padding: const EdgeInsets.all(25.0),
+                      child: Container(
+                        width: MediaQuery.of(context).size.height,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 5),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
+                                  Column(
+                                    children: [
+                                      Container(
+                                        margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(14.0),
+                                          child: Icon(Icons.account_box, size: 100,),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
                                   Container(
                                     margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(14.0),
-                                      child: Icon(Icons.account_box, size: 100,),
+                                    child: Column(
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Text("Nome")
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Text("Email@email.com")
+                                          ],
+                                        ),
+                                      ],
                                     ),
                                   ),
                                 ],
                               ),
-                              Container(
-                                margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                child: Column(
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Text("Nome")
-                                      ],
-                                    ),
-                                    Row(
-                                      children: [
-                                        Text("Email@email.com")
-                                      ],
-                                    ),
-                                  ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 2.0),
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  fixedSize: const Size(350, 45), 
+                                  primary: Color.fromARGB(255, 14, 174, 62),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                                  onPressed: () {
+                                    Navigator.of(context).pushNamed('/changeProfile');
+                                  }, 
+                                child: Text("ALTERAR PERFIL", style: TextStyle(fontSize: 15),
                                 ),
                               ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 2.0),
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              fixedSize: const Size(350, 45), 
-                              primary: Color.fromARGB(255, 14, 174, 62),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 20.0),
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  fixedSize: const Size(350, 45), 
+                                  primary: Color.fromARGB(255, 14, 174, 62),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                                onPressed: () {
+                                  Navigator.of(context).pushNamed('/helpProfile');
+                                }, 
+                                child: Text("AJUDA", style: TextStyle(fontSize: 15),
+                                ),
                               ),
                             ),
-                              onPressed: () {
-                                Navigator.of(context).pushNamed('/changeProfile');
-                              }, 
-                            child: Text("ALTERAR PERFIL", style: TextStyle(fontSize: 15),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 20.0),
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              fixedSize: const Size(350, 45), 
-                              primary: Color.fromARGB(255, 14, 174, 62),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 7.0),
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  fixedSize: const Size(350, 45), 
+                                  primary: Color.fromARGB(255, 14, 174, 62),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                                onPressed: () {
+                                  Navigator.of(context).pushNamedAndRemoveUntil('/',(Route< dynamic > route) => false );
+                                }, 
+                                child: Text("SAIR", style: TextStyle(fontSize: 15),
+                                ),
                               ),
                             ),
-                            onPressed: () {
-                              Navigator.of(context).pushNamed('/helpProfile');
-                            }, 
-                            child: Text("AJUDA", style: TextStyle(fontSize: 15),
-                            ),
-                          ),
+                          ],
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 7.0),
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              fixedSize: const Size(350, 45), 
-                              primary: Color.fromARGB(255, 14, 174, 62),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                            ),
-                            onPressed: () {
-                              Navigator.of(context).pushNamedAndRemoveUntil('/',(Route< dynamic > route) => false );
-                            }, 
-                            child: Text("SAIR", style: TextStyle(fontSize: 15),
-                            ),
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
                   ),
                 ),
